272af6f8f091dba6b31ddde6b6a1d120df2360f8
AlexanderZhao <zhaomengzhe@qq.com>
2021-04-19 23:34:06 +0800

    feat:split store

---
+++ vanilux.ts
@@ -0,0 +1,50 @@
+class Store {
+  reducer: Function;
+  curState: any;
+
+  constructor(reducer, initState) {
+    this.reducer = reducer;
+    this.curState = initState;
+  }
+
+  // 订阅者
+  subscribers = [];
+
+  // 获得当前state
+  getState() {
+    return this.curState;
+  }
+
+  // 添加订阅
+  subscribe(fn) {
+    this.subscribers = [...this.subscribers, fn];
+  }
+
+  // 分发消息
+  dispatch(action) {
+    this.curState = this.reducer(this.curState, action);
+    this.subscribers.forEach((fn) => fn(this.curState));
+  }
+}
+
+function todoReducer(state, action) {
+  // 深拷贝state以实现immutable
+  const stateCopy = JSON.parse(JSON.stringify(state));
+
+  switch (action.type) {
+    case "ADD_TODO":
+      return {
+        ...stateCopy,
+        todo: stateCopy.todo.concat(action.payload),
+      };
+    case "REMOVE_TODO":
+      return {
+        ...stateCopy,
+        todo: stateCopy.todo.filter((el) => el.key !== action.payload),
+      };
+    default:
+      return state;
+  }
+}
+
+const TodoStore = new Store(todoReducer, { todo: [] });
